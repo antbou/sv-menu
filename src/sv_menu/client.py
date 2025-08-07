c90e548267ba817672f464de5a162c9b08856b61
antho <bouillant.anthony@gmail.com>
2025-08-07 12:15:00 +0200

    Disable SSL verification

--- src/sv_menu/client.py
+++ src/sv_menu/client.py
@@ -1,14 +1,15 @@
-
-import certifi
 import requests
+import urllib3
 from sv_menu.types import Menu
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 ENDPOINT_URL = "https://svm-menu.fly.dev/v1/api/menus"
 
 def fetch_menus() -> list[Menu]:
     """Fetch menus from the API."""
 
-    response = requests.get(ENDPOINT_URL, headers={"Accept": "application/json"}, verify=certifi.where())
+    response = requests.get(ENDPOINT_URL, headers={"Accept": "application/json"}, verify=False)
     if response.status_code == 200:
         menus = response.json()
         return menus
